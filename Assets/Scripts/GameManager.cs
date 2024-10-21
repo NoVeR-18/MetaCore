6190d96c5098cecfc3b06d8df343ba863fb905a0
Alexander Chudin <chudinsasha1@gmail.com>
2024-10-21 13:55:22 +0300

    create base input for core mechanick

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+    public int collectedStars=0;
+    public string currentScene;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
